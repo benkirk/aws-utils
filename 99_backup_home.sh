0afc4daea2638019d39ce457fe019e67d6a46bc7
Benjamin Kirk <benkirk@ucar.edu>
2024-12-22 17:03:54 +0000

    POD

---
+++ 99_backup_home.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo rsync -axvP \
+     --exclude '.bashrc' \
+     --exclude '.docker/' \
+     --exclude '.cache/' \
+     /home/ec2-user/ /pv/ec2-user-home/
+
+echo && echo "[${0}] complete on $(date)"
