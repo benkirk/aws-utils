e2057417cf9587610f13c6596d1fa2b7ca46be83
Benjamin Kirk <benkirk@ucar.edu>
2025-01-03 23:48:32 +0000

    exclude ~/.cargo/

--- 99_backup_home.sh
+++ 99_backup_home.sh
@@ -4,6 +4,7 @@ sudo rsync -axvP \
      --exclude '.bashrc' \
      --exclude '.docker/' \
      --exclude '.cache/' \
+     --exclude '.cargo/' \
      /home/ec2-user/ /pv/ec2-user-home/
 
 echo && echo "[${0}] complete on $(date)"
